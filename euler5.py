c914e3ceac5f559d0227c15d3c4256a5749b2e0d
Mak2006 <gmcct.adm@gmail.com>
2020-06-08 22:56:52 +0100

    init for prob 5

--- euler5.py
+++ euler5.py
@@ -3,11 +3,5 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 """
-a combination of all prime factors must be there
-2, 3, 4,5, 6, 7, 8, 9, 10, 11, 13, 17, 19
-
-
-so the mul of these is
-
 
 """
