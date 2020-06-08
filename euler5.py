fb8a6b532d865d4f0246b74b29b1d0889911d19f
Mak2006 <gmcct.adm@gmail.com>
2020-06-08 22:56:25 +0100

    Palindrom method works optimized, works fine for 7 digits.

---
+++ euler5.py
@@ -0,0 +1,13 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+"""
+a combination of all prime factors must be there
+2, 3, 4,5, 6, 7, 8, 9, 10, 11, 13, 17, 19
+
+
+so the mul of these is
+
+
+"""
