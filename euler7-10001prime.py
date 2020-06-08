07d7925c4dfe774672f795209402d1c912770a05
Mak2006 <gmcct.adm@gmail.com>
2020-06-09 00:40:18 +0100

    renamed files

---
+++ euler7-10001prime.py
@@ -0,0 +1,13 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# 
+# What is the 10 001st prime number?
+
+
+def getprime(n):
+
+
+
+if __name__ == '__main__':
+    print("Enter n, program gives the nth prime")
+    n = int(input)
+    getprime(n)
