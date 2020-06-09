aa80053ebbaab03dc25154b0b48f5fd12805d883
Mak2006 <gmcct.adm@gmail.com>
2020-06-09 21:05:13 +0100

    prob 10 optimization

--- euler10.py
+++ euler10.py
@@ -1,8 +1,31 @@
 import eulerlib.mathlib as ml
 
-if __name__ == '__main__':
-    k = ml.factorize(1200)
-    print(k)
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+# Find the sum of all the primes below two million.
+#
+#I think 2 mil is 2 000 000
 
-    primelist = ml.getprime(1000)
-    print(primelist.sum())
+if __name__ == '__main__':
+    # k = ml.factorize(1200)
+    # print(k)
+
+    import cProfile
+    import profile
+
+    #cProfile.run('ml.getprimes_lessthan(2000)')
+    #profile.run('ml.getprimes_lessthan(2000); print')
+    #time =
+    k = 2000000
+    import time
+
+    start_time = time.time()
+    #primelist = ml.getprimes_lessthan(k)
+    primelist, sum = ml.getprimes_lessthan_array(k)
+    end_time = time.time()
+    tot_time = end_time - start_time
+
+    print("# speed = {} / prime for k {} numbers, total time {} sec".format(tot_time/k, k, tot_time))
+    print(primelist)
+    print("sum is {}".format(sum))
+    #print(primelist.sum())
--- eulerlib/mathlib.py
+++ eulerlib/mathlib.py
@@ -98,10 +98,63 @@ def getprime(n):
             fac[len(fac)] = num
     return fac
 
+# performance
+# 0.0006874 per prime for 2000
+
+def getprimes_lessthan(n):
+    fac = pd.Series([2])  # we just put the frist prime.
+    if n <= 1:
+        return fac
+
+    num = fac[len(fac) - 1]  # the last num we got
+    while num < n:  # till our number is < than n
+        # get the next number k
+        # if any of the numbers in fac divides k exclude k.
+        # else add k to fac
+        # loop over
+        num = num + 1  # the next number
+        divides = check(fac, num)
+        if not divides:
+            print("found a prime " + str(num))
+            fac[len(fac)] = num
+    return fac
+
+# perform - much better using just [] rather than pandas
+# 3.998041152954101e-06 / prime for 2000 prime
+# 0.0002474347138404846 / prime for 200000
+# speed = 3.5189390182495115e-06 / prime 2000
+# speed = 2.500176429748535e-06 / prime for k 2000 numbers
+# speed = 7.669986963272094e-05 / prime for k 100000 numbers
+# speed = 0.0003510916748046875 / prime for k 500000 numbers
+def getprimes_lessthan_array(n):
+    sum = 5
+    fac = [2, 3 ]  # we just put the frist prime.
+    if n <= 1:
+        return fac
+
+    num = fac[len(fac) - 1]  # the last num we got
+    while num < n:  # till our number is < than n
+        # get the next number k
+        # if any of the numbers in fac divides k exclude k.
+        # else add k to fac
+        # loop over
+        num = num + 2 # certainly next num is even so we can increment by 2
+        divides = check(fac, num)
+        if not divides:
+            #print("found a prime " + str(num))
+            fac.append(num)
+            sum = sum + num  # keep the sum as well
+        # to see am alive
+        if(num %10000 == 1):
+            print(num)
+    return fac, sum
 
 def check(fac, num):
-    for i in range(1, len(fac)):
+    k = len(fac)
+    for i in range(1, k):  # 1 becuase we dont need check with 2
+       # print("num {}, fac[i] {}".format(num, fac[i]))
         if num % fac[i] == 0:
             # this is a composite
-            return True
+            # print("am here")
+            return True # return on first occurance
     return False  # if we have come thus far , it means none of the exisitn nums, in fac were able to divide the new number, henc it is a prime.
