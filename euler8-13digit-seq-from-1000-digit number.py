72f4489b19999e12765ead3d06ed65ffb78584ce
Mak2006 <gmcct.adm@gmail.com>
2020-06-09 02:13:49 +0100

    Seems to be working now, done

--- euler8-13digit-seq-from-1000-digit number.py
+++ euler8-13digit-seq-from-1000-digit number.py
@@ -40,34 +40,78 @@ def break_num(num, length):
     pass
 
 
+def mul(seq):
+    res = 1
+    for i in range(0, len(seq)):
+        res = res * int(seq[i])
+    return res
+
+
+def add(seq):
+    res = 0
+    for i in range(0, len(seq)):
+        res = res + int(seq[i])
+    return res
+
+
+# compares seq and nextseq and return true if the digits sum of seq is > then that of nextseq
 def compare(seq, nextseq):
     # just get the sum of the numebrs within the sequence and compare
     # 12313 , 33441 1+2+3+1+3 vs 3 + 3+4 +4 +1
     # say True or False.
+    sa = add(seq)
+    sna = add(nextseq)
+    if (sa >= sna):
+        return True
+    else:
+        return False
 
 
-
+# Returns the needed seq
 def findseq(seq, num, length):
+    # seq is the existing list which holds the best answer we have.
     # num is the number
     # length is the chunks to be be broken into.
     # chunks = break_num(num, length) do noth think it will be a good idea to store the chunks
 
-    #and we use recursion
-    #we introduce a seq variable which holds the last useful seq.
+    # and we use recursion
+    # we introduce a seq variable which holds the last useful seq.
     # we change the invoke in main and pass an empty []
     # we assume the next sequence to be taken starts from the first char of num
-    nextseq = num[0:length - 1]
+    if (len(num) >= length):
+        nextseq = num[0:length ]  # get the first sequen of length chars
+    else:
+        # no more chunks exists and seq is the required seq
+        return seq
+
+    # at this time more exists
     print("next seq " + str(nextseq))
 
     # now we require a comparator
+    # returns true if seq > nextseq as per the compare function.
     cres = compare(seq, nextseq)
+    if (cres):
+        # this is the reduction step.
+        # knock of the first digit and call recurse
+        num = num[1:len(num)]
 
+    else:
+        seq = nextseq  # as nextseq is greater
 
+    # go on to compare with more seq
+    seq = findseq(seq, num, length)
+
+    return seq
 
 
 if __name__ == '__main__':
     # We probably have to take this num as a string
     # num = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
     num = "1234135940"  # test num
-
-    seq = findseq([], num, 3)
+    seq = []  # this is the seq that is of interest
+    # for now we do not knwo what it is
+    seq = findseq(seq, num, 4)  # num is the target num and 3 is the chunk size
+    print("Seq is " + seq)
+    # Now do what we want to do
+    res = mul(seq)
+    print("Required value " + str(res))
